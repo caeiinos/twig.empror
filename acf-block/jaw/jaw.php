20a107f4548df4c7a8d7871fa60ca6498cf4be6d
caeiinos <jeanderoy2001@gmail.com>
2023-09-23 17:56:34 +0200

    need to finish block structure

--- acf-block/jaw/jaw.php
+++ acf-block/jaw/jaw.php
@@ -10,8 +10,7 @@ $id = 'jaw--' . $block['id'];
 
 $jaw = get_field('jaw');
 
-if ($jaw) {
-?>
+if ($jaw) { ?>
 <section id="<?php echo $id; ?>" class="jaw jaw--<?php echo esc_attr($jaw['style']); ?>">
     <h2 class="jaw__title jaw__title--<?php echo esc_attr($jaw['style']); ?>">
         <?php echo esc_html($jaw['title']); ?>
@@ -19,11 +18,18 @@ if ($jaw) {
     <div class="jaw__text jaw__text--<?php echo esc_attr($jaw['style']); ?>">
         <?php echo esc_html($jaw['text']); ?>
     </div>
-    <div class="jaw__containerimg1 jaw__containerimg1--<?php echo esc_attr($jaw['style']); ?>">
-        <img class="jaw__img1 jaw__img1--<?php echo esc_attr($jaw['style']); ?>" src="<?php echo esc_url($jaw['img_1']['url']); ?>" alt="<?php echo esc_attr($jaw['img_1']['alt']); ?>">
-    </div>
-    <video>
-        <source src="<?php echo esc_url($jaw['video']['url']); ?>" type="video/mp4" >
-    </video>
+    <?php if ($jaw['type'] == 'image') { ?>
+        <div class="jaw__containerimg1 jaw__containerimg1--<?php echo esc_attr($jaw['style']); ?>">
+            <img class="jaw__img1 jaw__img1--<?php echo esc_attr($jaw['style']); ?>" src="<?php echo esc_url($jaw['img_1']['url']); ?>" alt="<?php echo esc_attr($jaw['img_1']['alt']); ?>">
+        </div>        
+    <?php }
+else { ?>
+        <div class="jaw__containerimg1 jaw__containerimg1--<?php echo esc_attr($jaw['style']); ?>">
+            <?php echo esc_url($jaw['video']['url']); ?>
+        </div>
+        <video autoplay muted loop class="jaw__containerimg1 jaw__containerimg1--<?php echo esc_attr($jaw['style']); ?>">
+            <source src="<?php echo esc_url($jaw['video']['url']); ?>" type="video/mp4" >
+        </video>        
+    <?php }; ?>
 </section>
 <?php }; ?>
